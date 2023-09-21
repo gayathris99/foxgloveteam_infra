419a3dc59b679137713ce26f04c412364587f6ca
gayathris99 <gayathri11499@gmail.com>
2023-09-21 16:09:18 +0530

    Login Logout Functionalities

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,44 +1,77 @@
 <template>
-  <q-page class="flex flex-center">
-    Foxglove Infra
+  <q-page class="" v-if="isUser">
+    <q-header class="q-py-sm q-px-md row items-center justify-between">
+      <div class="row items-center justify-between q-gutter-md">
+        <img src="~assets/favicon-128x128.png" width="50"/>
+        <div class="fs-20 fw-700">Foxglove Team Infra</div>
+      </div>
+      <q-btn class="fw-700" label="logout" style="background-color:white; color:#004953" @click="onLogout"></q-btn>
+    </q-header>
+    <q-page-container class="no-padding q-mt-xl">
+      <div v-for="(instance, id) in instances" :key="id" class="q-px-lg">
+        <div class="row items-center q-my-lg q-gutter-x-lg text-primary">
+          <div class="fw-700 fs-18" style="width:200px"><span class="fs-14">{{id+1}}).</span> {{instance.name}}</div>
+          <div class="fw-500 fs-14"  style="width:200px">{{instance.currentStatus}}</div>
+          <q-btn label="Start" color="primary" class="fw-600"></q-btn>
+          <q-btn label="Stop" color="primary" class="fw-600"></q-btn>
+        </div>
+      </div>
+    </q-page-container>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 
 export default defineComponent({
   name: 'IndexPage',
-  // methods: {
-  //   loginUser () {
-  //   const email = 'nisarga@foxgloveteam.com'
-  //   const password = 'teampassword'
-  //   const auth = getAuth();
-  //   signInWithEmailAndPassword(auth, email, password)
-  //     .then((userCredential) => {
-  //       console.log(auth.currentUser)
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code;
-  //       const errorMessage = error.message;
-  //     });
-  //   }
-  // },
-  // async mounted () {
-  //   // await this.loginUser()
-  //   // const auth = getAuth();
-  //   // signOut(auth).then(() => {
-  //   // }).catch((error) => {
-  //   //   // An error happened.
-  //   // });
-  //   // console.log(auth.currentUser)
-  // }
-    beforeCreate () {
-      const auth = getAuth()
-      if (!auth.currentUser) {
+  data () {
+    return {
+      isUser: false,
+      instances: [
+        {
+          name: 'Instance 1',
+          currentStatus: 'Running'
+        },
+        {
+          name: 'Instance 2',
+          currentStatus: 'Stopped'
+        },
+        {
+          name: 'Instance 3',
+          currentStatus: 'Stopped'
+        },
+        {
+          name: 'Instance 4',
+          currentStatus: 'Running'
+        },
+      ]
+    }
+  },
+  methods: {
+    onLogout () {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      this.$router.push({ name: 'login' })
+    }).catch((error) => {
+      // An error happened.
+      this.$q.notify({
+        message: error.message,
+        color: "red",
+        position: "top"
+      });    });
+    }
+  },
+  beforeCreate () {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.isUser = true
+      } else {
         this.$router.push({ name: 'login' })
       }
-    }
+    });
+  }
 })
 </script>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,20 +1,83 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center" v-if="!isUser">
     <q-card>
       <q-card-section>
         <div class="fw-700 fs-24 text-primary">Login</div>
         <div class="q-mt-md">
-          <q-input square outlined v-model="email" label="Email" class="fs-16" />
-          <q-input square outlined v-model="password" label="Password" type="password" class="q-mt-md fs-16"/>
-          <div class="flex flex-center">
-            <q-btn label="Login" color="primary" class="fw-700 q-mt-lg"></q-btn>
-          </div>
+          <q-form @submit.prevent.stop="onLogin">
+            <q-input square outlined v-model="email" label="Email" class="fs-16" :rules="[val => !!val || 'Email is required']"/>
+            <q-input square outlined v-model="password" label="Password" type="password" class="q-mt-sm fs-16" :rules="[val => !!val || 'Password is required']"/>
+            <div class="flex flex-center">
+              <q-btn label="Login" color="primary" type="submit" class="fw-700 q-mt-md"></q-btn>
+            </div>
+          </q-form>
         </div>
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
+
+<script>
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      email: '',
+      password: '',
+      isUser: true
+    }
+  },
+  methods: {
+    async onLogin (e) {
+      e.preventDefault()
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          console.log(auth.currentUser)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          this.$q.notify({
+            message: errorMessage,
+            color: "red",
+            position: "top"
+          });
+        });
+    }
+  },
+  beforeCreate () {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.isUser = true
+        this.$router.push({ name: 'home' })
+      } else {
+        this.isUser = false
+        // this.$router.push({ name: 'login' })
+      }
+    });
+  }
+  // mounted () {
+  //   const auth = getAuth();
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // User is signed in, see docs for a list of available properties
+  //       // https://firebase.google.com/docs/reference/js/auth.user
+  //       const uid = user.uid;
+  //       console.log(user)
+  //       // ...
+  //     } else {
+  //       // User is signed out
+  //       // ...
+  //     }
+  //   });
+
+  // }
+}
+</script>
 <style lang="scss" scoped>
 .q-page {
   background: #004953;
